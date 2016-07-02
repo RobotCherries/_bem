082c130701abde7273cd83f6699dca3dc9b4f34d
Silvestar Bistrovic <malimirkeccita@gmail.com>
2016-07-02 17:23:23 +0200

    Adding bem classes to comment form.

--- comments.php
+++ comments.php
@@ -78,11 +78,70 @@ if ( post_password_required() ) {
 		<p class="no-comments _comments__no-comment"><?php esc_html_e( 'Comments are closed.', '_bem' ); ?></p>
 	<?php
 	endif;
+  
+  $fields =  array(
+    'author' =>
+      '<p class="comment-form-author _comments__field"><label for="author" class="_comments__field-label">' . __( 'Name', 'domainreference' ) . '</label> ' .
+      ( $req ? '<span class="required _comments__field-required">*</span>' : '' ) .
+      '<input class="_comments__field-input _comments__field-input--name" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+      '" size="30"' . $aria_req . ' /></p>',
 
-	comment_form(array(
-		'class_form' => '_comments__form',
-		'class_submit' => '_comments__form-submit'
-	));
+    'email' =>
+      '<p class="comment-form-email _comments__field"><label for="email" class="_comments__field-label">' . __( 'Email', 'domainreference' ) . '</label> ' .
+      ( $req ? '<span class="required _comments__field-required">*</span>' : '' ) .
+      '<input class="_comments__field-input _comments__field-input--email" id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+      '" size="30"' . $aria_req . ' /></p>',
+
+    'url' =>
+      '<p class="comment-form-url _comments__field"><label for="url" class="_comments__field-label">' . __( 'Website', 'domainreference' ) . '</label>' .
+      '<input class="_comments__field-input _comments__field-input--website" id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+      '" size="30" /></p>',
+  );
+  
+  comment_form(array(
+    'id_form' => 'commentform',
+    'class_form' => 'comment-form _comments__form',
+    'id_submit' => 'submit',
+    'class_submit' => 'submit _comments__form-submit',
+    'name_submit' => 'submit',
+    'title_reply' => __( 'Leave a Reply' ),
+    'title_reply_to' => __( 'Leave a Reply to %s' ),
+    'cancel_reply_link' => __( 'Cancel Reply' ),
+    'label_submit' => __( 'Post Comment' ),
+    'format' => 'xhtml',
+    'comment_field' =>  '<p class="comment-form-comment _comments__field"><label for="comment" class="_comments__field-label">' . _x( 'Comment', 'noun' ) .
+      '</label><textarea class="_comments__field-input _comments__field-input--comment" id="comment" name="comment" cols="45" rows="8" aria-required="true">' .
+      '</textarea></p>',
+    'must_log_in' => '<p class="must-log-in _comments__warning _comments__warning--not-logged-in">' .
+      sprintf(
+        __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+        wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+      ) . '</p>',
+  
+    'logged_in_as' => '<p class="logged-in-as _comments__user">' .
+      sprintf(
+      __( 'Logged in as <a href="%1$s" class="_comments__user-name">%2$s</a>. <a href="%3$s" class="_comments__logout-link" title="Log out of this account">Log out?</a>' ),
+        admin_url( 'profile.php' ),
+        $user_identity,
+        wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+      ) . '</p>',
+  
+    'comment_notes_before' => '<p class="comment-notes _comments__notes">' .
+      __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+      '</p>',
+  
+    'comment_notes_after' => '<p class="form-allowed-tags _comments__notes">' .
+      sprintf(
+        __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s' ),
+        ' <code class="_comments__notes-code">' . allowed_tags() . '</code>'
+      ) . '</p>',
+  
+    'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+  ));
+  #comment_form(array(
+	#	'class_form' => '_comments__form',
+	#	'class_submit' => '_comments__form-submit'
+	#));
 	?>
 
 </div><!-- #comments -->
