99884cc3e1ca1d1bf25c556c7993dad526c2bd7d
Star Bist <malimirkeccita@gmail.com>
2017-01-28 15:02:27 +0100

    Adding critical css in bem theme.

--- functions.php
+++ functions.php
@@ -120,14 +120,14 @@ add_action( 'widgets_init', '_bem_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _bem_scripts() {
-	wp_enqueue_style( '_bem-style', get_stylesheet_uri() );
+	wp_enqueue_style( '_bem-style', get_template_directory() . '/dist/style.css' );
 
-	wp_enqueue_script( '_bem-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	// wp_enqueue_script( '_bem-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( '_bem-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// wp_enqueue_script( '_bem-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+		// wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', '_bem_scripts' );
