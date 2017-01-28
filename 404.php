ad153db0db2b17f3ee0e23f548944d5730b91216
Star Bist <malimirkeccita@gmail.com>
2017-01-28 13:44:27 +0100

    Replacing 404 page.

--- 404.php
+++ 404.php
@@ -18,42 +18,10 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content _content__article-content _content__article-content--404">
-					<p class="_content__404-text"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_bem' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-					if ( _bem_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories _content__categories _content__category--404">
-					<h2 class="widget-title _content__category-title _content__category-title--404"><?php esc_html_e( 'Most Used Categories', '_bem' ); ?></h2>
-					<ul class="_content__category-list">
-					<?php
-					wp_list_categories( array(
-						'orderby'    => 'count',
-						'order'      => 'DESC',
-						'show_count' => 1,
-						'title_li'   => '',
-						'number'     => 10,
-					) );
-				?>
-				</ul>
-				</div><!-- .widget -->
-
-				<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p class="_content__404-text">' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', '_bem' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					<p class="_content__404-text"><?php esc_html_e( 'It looks like nothing was found at this location. Are you "trying" to find something funny?', '_bem' ); ?></p>
+                    <div class="_content__404-video">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/kkuhGHFnvSU" frameborder="0" allowfullscreen></iframe>
+                    </div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
@@ -61,4 +29,7 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
+if ( has_post_format( 'aside' ) ) {
+	get_sidebar();
+}
 get_footer();
